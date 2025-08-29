f944dc6fa2d26477d1255e9d80f9365bc00a745e
Ayush Grover <agayushgrover@Ayushs-MacBook-Air.local>
2025-08-29 23:44:10 +0530

    Vercel link added

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ const cors = require("cors");
 //   credentials: true
 // }));
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: "https://kutumb-ruddy.vercel.app", 
   credentials: true
 }));
 
